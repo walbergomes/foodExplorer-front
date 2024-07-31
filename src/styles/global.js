06aff157fc12cc022df6002e6b15d8be6f6a96f1
Walber <walberg.s48@gmail.com>
2024-07-31 17:52:33 -0300

    :iphone: defining font to large mobiles

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from "styled-components"
 
+import { DEVICE_BREAKPOINTS } from "./deviceBreakPoints"
+
 export default createGlobalStyle`
   * {
     margin: 0;
@@ -12,9 +14,12 @@ export default createGlobalStyle`
   }
 
   body {
-    font-size: 1.6rem;
     background-color: ${({ theme }) => theme.COLORS.DARK_400};
     --webkit-font-smoothing: antialiased;
+
+    @media (min-width: ${DEVICE_BREAKPOINTS.MOBILE_L}) {
+      font-size: 1.6rem;
+    }
   }
 
   a {
