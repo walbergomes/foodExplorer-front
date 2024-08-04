cb20e46aad241ff0c5a52188715d1be8f2f7532a
Walber <walberg.s48@gmail.com>
2024-08-04 17:59:31 -0300

    :construction: created base Dish prate

---
+++ src/pages/Dish/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+import { Header } from "../../components/Header"
+
+export function Dish() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  )
+}
