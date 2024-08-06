dd58936646cdf62251e04b29e161d9843b7a51e5
Walber <walberg.s48@gmail.com>
2024-08-06 16:20:14 -0300

    :lipstick: finished Edit Dish page

---
+++ src/pages/EditDish/index.jsx
@@ -0,0 +1,78 @@
+import {
+  Container,
+  Content,
+  BackButton,
+  Wrapper,
+  FileInput,
+  IngredientsArea,
+  ButtonsArea,
+} from "./styles";
+
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { Select } from "../../components/Select";
+import { IngredientCreate } from "../../components/IngredientCreate";
+import { TextArea } from "../../components/TextArea";
+import { Button } from "../../components/Button";
+import { Footer } from "../../components/Footer";
+
+import { CaretLeft, UploadSimple } from "@phosphor-icons/react";
+
+export function EditDish() {
+  return (
+    <Container>
+      <Header />
+      <Content>
+        <BackButton>
+          <CaretLeft />
+          <span>voltar</span>
+        </BackButton>
+
+        <h2>Novo prato</h2>
+
+        <Wrapper>
+          <label htmlFor="add-dish">Imagem do prato</label>
+          <FileInput>
+            <UploadSimple />
+            Selecione imagem para alterá-la
+            <input type="file" id="add-dish" />
+          </FileInput>
+        </Wrapper>
+
+        <Wrapper>
+          <label htmlFor="name">Nome</label>
+          <Input type="text" id="name" placeholder="Ex.: Salada Ceasar" />
+        </Wrapper>
+
+        <Wrapper>
+          <label htmlFor="">Categorias</label>
+          <Select />
+        </Wrapper>
+
+        <Wrapper>
+          <label htmlFor="">Ingredientes</label>
+          <IngredientsArea>
+            <IngredientCreate value="Pão Naan" />
+            <IngredientCreate isNew placeholder="Adicionar" />
+          </IngredientsArea>
+        </Wrapper>
+
+        <Wrapper>
+          <label htmlFor="">Preço</label>
+          <Input type="number" placeholder="R$ 40,00" />
+        </Wrapper>
+
+        <Wrapper>
+          <label htmlFor="">Descrição</label>
+          <TextArea placeholder="A Salada César é uma opção refrescante para o verão." />
+        </Wrapper>
+
+        <ButtonsArea>
+          <Button backgroundColor="DARK_800" title="Excluir prato" />
+          <Button backgroundColor="TOMATO_400" title="Salvar alterações" />
+        </ButtonsArea>
+      </Content>
+      <Footer />
+    </Container>
+  );
+}
---
+++ src/pages/EditDish/styles.js
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+
+  width: 100%;
+  padding: 0.625rem 3.375rem;
+
+  > h2 {
+    color: ${({ theme }) => theme.COLORS.LIGHT_300};
+    font-weight: 500;
+    font-family: "Poppins", sans-serif;
+    font-size: 2rem;
+  }
+`;
+
+export const BackButton = styled.button`
+  display: flex;
+  align-items: center;
+
+  background: none;
+  border: none;
+
+  color: ${({ theme }) => theme.COLORS.LIGHT_300};
+  font-weight: 500;
+  font-size: 1rem;
+
+  > svg {
+    font-size: 1.5rem;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  > label {
+    color: ${({ theme }) => theme.COLORS.LIGHT_400};
+  }
+`;
+
+export const FileInput = styled.label`
+  display: flex;
+  align-items: center;
+  gap: .5rem;
+
+  width: 100%;
+  height: 3rem;
+  
+  background-color: ${({ theme }) => theme.COLORS.DARK_900};
+  border-radius: .8rem;
+
+  color: ${({ theme }) => theme.COLORS.LIGHT_100};
+  font-size: .875rem;
+
+  > svg {
+    color: ${({ theme }) => theme.COLORS.LIGHT_100};
+    font-size: 1.5rem;
+    margin-left: .875rem;
+  }
+
+  > input {
+    display: none;
+  }
+`;
+
+export const IngredientsArea = styled.div`
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+
+  width: 100%;
+  background-color: ${({ theme }) => theme.COLORS.DARK_900};
+  border-radius: .5rem;
+  padding: .5rem;
+`
+
+export const ButtonsArea = styled.div`
+  display: flex;
+  gap: 2rem;
+`
