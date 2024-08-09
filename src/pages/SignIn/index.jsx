ece864a51fa42ef50a32069184247be7202e58a9
Walber <walberg.s48@gmail.com>
2024-08-08 22:42:57 -0300

    :iphone: SignIn Page - adjusting responsiveness for screens larger than 1024px

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,23 +1,41 @@
 import { Container, Form, Wrapper } from "./styles";
 
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakPoints"
+
 import logoImage from "../../assets/logoImage.svg";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export function SignIn() {
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= parseInt(DEVICE_BREAKPOINTS.LAPTOP));
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= parseInt(DEVICE_BREAKPOINTS.LAPTOP));
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Limpa o event listener quando o componente é desmontado
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <Container>
       <img src={logoImage} alt="logo food explorer" />
 
       <Form>
+      {isLargeScreen && <h1>Faça Login</h1>}
+      
         <Wrapper>
           <label htmlFor="email">Email</label>
           <Input
             id="email"
             type="email"
-            placeholder="Exemplo: exemplo@exemplo.com.br" 
+            placeholder="Exemplo: exemplo@exemplo.com.br"
           />
         </Wrapper>
 
@@ -26,13 +44,12 @@ export function SignIn() {
           <Input
             type="password"
             id="password"
-            placeholder="No mínimo 6 caracteres" 
+            placeholder="No mínimo 6 caracteres"
           />
         </Wrapper>
-        <Button title="Entrar" backgroundColor="TOMATO_100"/> 
+        <Button title="Entrar" backgroundColor="TOMATO_100" />
+        <Link to="/register">Criar uma conta</Link>
       </Form>
-      
-      <Link to="/register">Criar uma conta</Link>
     </Container>
   );
 }
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakPoints.js"
 
 export const Container = styled.div`
   display: flex;
@@ -13,11 +14,12 @@ export const Container = styled.div`
     margin-bottom: 4.5rem;
   }
 
-  a {
-    color: ${({ theme }) => theme.COLORS.LIGHT_100};
-    font-size: .875rem;
-    text-align: center;
-    margin-top: 2rem;
+  @media (min-width: ${DEVICE_BREAKPOINTS.LAPTOP}) {
+   flex-direction: row;
+   align-items: center;
+   justify-content: space-between;
+
+   margin: 0 7rem;
   }
 `;
 
@@ -25,6 +27,27 @@ export const Form = styled.form`
   display: flex;
   flex-direction: column;
   gap: 2rem;
+
+  > h1 {
+    color: ${({ theme }) => theme.COLORS.LIGHT_100};
+    font-family: "Poppins", sans-serif;
+    font-size: 2rem;
+    font-weight: 500;
+    text-align: center;
+  }
+
+  a {
+    color: ${({ theme }) => theme.COLORS.LIGHT_100};
+    font-size: .875rem;
+    text-align: center;
+  }
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.LAPTOP}) {
+    background-color: ${({ theme }) => theme.COLORS.DARK_700};
+    padding: 4rem;
+    border-radius: 1rem;
+    min-width: 29rem;
+  }
 `
 
 export const Wrapper = styled.div`
