b972f2f5da59e015b91594147a1033f59560ba69
Walber <walberg.s48@gmail.com>
2024-10-15 21:30:50 -0300

    :lipstick: adjust button component - add class

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,10 @@
 import { Container } from "./styles"
 
-export function Button({ icon: Icon, title, bg, ...rest}) {
+export function Button({ icon: Icon, title, bg, className, ...rest}) {
   return (
     <Container 
       $bg={bg}
+      className={className}
     >
       { Icon && <Icon size={20} />}
       <button {...rest}>{title}</button>
