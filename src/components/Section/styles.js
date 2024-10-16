4c4f369e7a4d14880a89cd29c39c8613fabf9d73
Walber <walberg.s48@gmail.com>
2024-10-15 22:05:12 -0300

    :iphone: adjust responsiveness Section component

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -1,9 +1,15 @@
 import styled from "styled-components";
+import { DEVICES_BREAKPOINTS } from "../../styles/devicesBreakpoints";
 
 export const Container = styled.div`
   > h2 {
     font-size: 1.125rem;
     font-weight: 500;
-    margin: 1.5rem 0 ;
+    margin: 1.5rem 0;
+    color: ${({ theme }) => theme.COLORS.LIGHT_300};
+
+    @media (min-width: ${DEVICES_BREAKPOINTS.TABLET}) {
+      font-size: 2rem;
+    }
   }
 `;
